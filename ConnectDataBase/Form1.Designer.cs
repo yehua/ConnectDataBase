deac5225276aa6ee0dbc4d7550f1bf9eb0e885a7
guojian <guojingjian@LIVE.CN>
2016-08-13 16:31:08 +0800

    建立链接

--- ConnectDataBase/Form1.Designer.cs
+++ ConnectDataBase/Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fpSpread1 = new FarPoint.Win.Spread.FpSpread();
+            this.fpSpread1_Sheet1 = new FarPoint.Win.Spread.SheetView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1_Sheet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "姓名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "年龄：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 23);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(617, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 42);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.fpSpread1);
+            this.panel1.Location = new System.Drawing.Point(0, 112);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(897, 439);
+            this.panel1.TabIndex = 5;
+            // 
+            // fpSpread1
+            // 
+            this.fpSpread1.AccessibleDescription = "fpSpread1, Sheet1, Row 0, Column 0, 姓名";
+            this.fpSpread1.Location = new System.Drawing.Point(3, 3);
+            this.fpSpread1.Name = "fpSpread1";
+            this.fpSpread1.Sheets.AddRange(new FarPoint.Win.Spread.SheetView[] {
+            this.fpSpread1_Sheet1});
+            this.fpSpread1.Size = new System.Drawing.Size(891, 433);
+            this.fpSpread1.TabIndex = 0;
+            // 
+            // fpSpread1_Sheet1
+            // 
+            this.fpSpread1_Sheet1.Reset();
+            fpSpread1_Sheet1.SheetName = "Sheet1";
+            // Formulas and custom names must be loaded with R1C1 reference style
+            this.fpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.R1C1;
+            fpSpread1_Sheet1.RowCount = 10;
+            this.fpSpread1_Sheet1.Cells.Get(0, 0).Value = "姓名";
+            this.fpSpread1_Sheet1.Cells.Get(0, 1).Value = "ID号";
+            this.fpSpread1_Sheet1.Cells.Get(0, 2).Value = "性别";
+            this.fpSpread1_Sheet1.Cells.Get(0, 3).Value = "生日";
+            this.fpSpread1_Sheet1.Cells.Get(0, 4).Value = "地址";
+            this.fpSpread1_Sheet1.Cells.Get(0, 5).Value = "电话";
+            this.fpSpread1_Sheet1.ColumnHeader.Visible = false;
+            this.fpSpread1_Sheet1.Columns.Get(1).Width = 116F;
+            this.fpSpread1_Sheet1.Columns.Get(3).Width = 86F;
+            this.fpSpread1_Sheet1.Columns.Get(4).Width = 144F;
+            this.fpSpread1_Sheet1.RowHeader.Columns.Default.Resizable = false;
+            this.fpSpread1_Sheet1.ReferenceStyle = FarPoint.Win.Spread.Model.ReferenceStyle.A1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(896, 546);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "查询";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fpSpread1_Sheet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private FarPoint.Win.Spread.FpSpread fpSpread1;
+        private FarPoint.Win.Spread.SheetView fpSpread1_Sheet1;
     }
 }
 
--- ConnectDataBase/Program.cs
+++ ConnectDataBase/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace ConnectDataBase
 {
@@ -11,12 +13,26 @@ namespace ConnectDataBase
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
+        /// 
+        public static SqlConnection mycon;
         [STAThread]
+       
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            ConnectDataBase();
             Application.Run(new Form1());
+        }
+        /// <summary>
+        ///链接数据库 
+        /// </summary>
+        private static void ConnectDataBase()
+        {
+            string con, sql;
+            con = "Server=.;Database=shifenzheng;Trusted_Connection=SSPI";
+            sql = "select * from lianxi";
+            SqlConnection mycon = new SqlConnection(con);
         }
     }
 }
